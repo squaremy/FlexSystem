49eaa28eefa5d6525b0f73538a296df161fe4460
jeremymdunne@gmail.com <jeremymdunne@gmail.com>
2019-05-14 13:51:09 -0400

    updated teachers.json

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
-var teacherlist = document.getElementbyId("teachertable");
-	let newRow = teacherlist.insertRow(-1);
-	newRow.innerHTML = "working";
+//var teacherlist = document.getElementbyId("teachertable");
+//	let newRow = teacherlist.insertRow(-1);
+//	newRow.innerHTML = "working";
+
+
+function searchForTeacher() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://github.com/squaremy/FlexSystem/tree/master/configs/teachers.json');
+	xhr.responseType = 'json';
+	xhr.send();
+	xhr.onload = function() {
+		var list = xhr.response;
+		displayTeachers(list["teachers"]);
+	}
+}
+
+function displayTeachers(teachers) {
+	document.getElementById("searchtxt").innerHTML = teachers;
+}
+
+searchForTeacher();
