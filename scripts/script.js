96876dd538110b9ed8a64f9db5b8a81d80d29b2b
grantg2020 <abeach2010@gmail.com>
2019-06-25 20:14:17 -0400

    Working test list with auto filtering added

--- scripts/script.js
+++ scripts/script.js
@@ -56,4 +56,20 @@ function displayTeachers(teachers) {
 	}
 }
 
+function searchFilter() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
 //searchForTeacher();
