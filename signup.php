9acf46f471787564cc1d653a58ef5cc7b7867a87
squaremy <martinj1@franklinacademy.org>
2019-07-31 20:52:19 -0400

    styling updates

--- signup.php
+++ signup.php
@@ -15,22 +15,24 @@
 	</div>
 	<div id="signupmenu">
 		<p id="searchtxt"><?php echo $_GET["name"]; ?></p>
-		<table id="weektable" style="display:block; top:30%">
-		  <tr>
-			<th width="15%">Monday</th>
-			<th width="15%">Tuesday</th>
-			<th width="15%">Wednesday</th>
-			<th width="15%">Thursday</th>
-			<th width="15%">Friday</th>
-		  </tr>
-		  <tr>
-			<td><input type="checkbox" name="mon" value="Monday"></td>
-			<td><input type="checkbox" name="tue" value="Tuesday"></td>
-			<td><input type="checkbox" name="wed" value="Wednesday"></td>
-			<td><input type="checkbox" name="thu" value="Thursday"></td>
-			<td><input type="checkbox" name="fri" value="Friday"></td>
-		  </tr>
-		</table>
+		<div id="weektable">
+			<table id="weektable" style="display:block; top:30%">
+			  <tr>
+				<th width="15%">Monday</th>
+				<th width="15%">Tuesday</th>
+				<th width="15%">Wednesday</th>
+				<th width="15%">Thursday</th>
+				<th width="15%">Friday</th>
+			  </tr>
+			  <tr>
+				<td><input type="checkbox" name="mon" value="Monday"></td>
+				<td><input type="checkbox" name="tue" value="Tuesday"></td>
+				<td><input type="checkbox" name="wed" value="Wednesday"></td>
+				<td><input type="checkbox" name="thu" value="Thursday"></td>
+				<td><input type="checkbox" name="fri" value="Friday"></td>
+			  </tr>
+			</table>
+		</div>
 		<table id="studenttable" border=1>
 		</table>
 	</div>
