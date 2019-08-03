e8a25c2ebdc60c9e307fcf599dabce31f4644eb8
grantg2020 <abeach2010@gmail.com>
2019-08-03 09:06:14 -0400

    Test

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,4 +1,6 @@
-/*{
+/*
+OLD code... unneccessary
+{
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId());
   console.log('Name: ' + profile.getName());
@@ -15,51 +17,10 @@
 	window.location.href = JSON.parse(sessionStorage.getItem('prevPage'));
   alert(profile.getName());
 }
+*/
+
 
 function checkIfLoggedIn(){
-//Use userdata to check login state.
-/*
-var userdata = readJSON('https://raw.githubusercontent.com/squaremy/FlexSystem/master/configs/userdata.json');
-console.log("userdata");
-console.log(userdata);
-if(userdata['user']){
-  window.location.href = "signin.html";
-}*/
-var userid = {};
-  try{
-    userid = sessionStorage.getItem('myUserEntity');
-    console.log(userid);
-    if(userid != null){
-      console.log("Signed in");
-      var profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId());
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-      var userEntity = {};
-      userEntity = JSON.parse(sessionStorage.getItem('myUserEntity'));
-    }else{
-      console.log("Not signed in");
-      if(window.location.href != "signin.html"){
-        sessionStorage.setItem('prevPage', JSON.stringify(window.location.href));
-      }else{
-        sessionStorage.setItem('prevPage', JSON.stringify("index.html"));
-      }
-        //sessionStorage.setItem('prevPage', JSON.stringify(window.location.href));
-        //window.location.href='signin.html';
-    }
-  }
-  catch(err){
-    console.log("Not signed in");
-    if(window.location.href != "signin.html"){
-      sessionStorage.setItem('prevPage', JSON.stringify(window.location.href));
-    }else{
-      sessionStorage.setItem('prevPage', JSON.stringify("index.html"));
-    }
-      //sessionStorage.setItem('prevPage', JSON.stringify(window.location.href));
-      //window.location.href='signin.html';
-  }
-  /*
   if(sessionStorage.getItem('myUserEntity') == null){
     console.log("NO USER-- Redirect to SignIn.html");
     //Redirect to login page, no user entity available in sessionStorage
@@ -76,6 +37,7 @@ var userid = {};
     userEntity = JSON.parse(sessionStorage.getItem('myUserEntity'));
   }*/
 }
+
 function logout()
 {
   //Don't forget to clear sessionStorage when user logs out
