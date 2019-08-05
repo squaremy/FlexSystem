7b05461bea7aa684e47ff037bdd73e70d337f4dd
squaremy <martinj1@franklinacademy.org>
2019-08-05 09:59:38 -0400

    test 2

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -107,4 +107,4 @@ function checktype(teacherelist){ //Check if student or teacher
 	name.innerHTML = userEntity["Name"];
 }
 
-read2JSON('https://raw.githubusercontent.com/squaremy/FlexSystem/master/configs/teacherlist.json');
+// read2JSON('https://raw.githubusercontent.com/squaremy/FlexSystem/master/configs/teacherlist.json');
