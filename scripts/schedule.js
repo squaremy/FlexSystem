9411386e3b322d37609cf6b4f03699dbf0bc218e
squaremy <martinj1@franklinacademy.org>
2019-08-07 10:27:27 -0400

    removed cell lines from titles?

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -81,6 +81,7 @@ function displayStudents(data) {
 		for(var j = 0; j < 3; j++) {
 			var cell = row.insertCell(-1);
 			if(i > -1) {
+				students.display = "table";
 				if(j == 1) {
 					cell.style.padding = "5px 5px 5px 5px";
 					cell.id = students[i];
@@ -97,6 +98,7 @@ function displayStudents(data) {
 					cell.innerHTML = "GOING TO";
 				}
 			} else {
+				students.display = "block";
 				if(j == 0) {
 					cell.innerHTML = "Kick?";
 				} else if(j == 1) {
@@ -115,6 +117,7 @@ function displayStudents(data) {
 			cell.style.padding = "5px 5px 5px 5px";
 			cell.id = visit[i];
 			if(i > -1) {
+				visit.display = "table";
 				if(visit[i] == null) {
 					cell.style.width = "20%";
 					cell.style.display = "none";
@@ -128,6 +131,7 @@ function displayStudents(data) {
 					} else cell.innerHTML = "COMING FROM";
 				}
 			} else {
+				visit.display = "block";
 				if(j == 0) {
 					cell.innerHTML = "Kick?";
 				} else if(j == 1) {
