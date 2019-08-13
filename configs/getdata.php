949b1679374bcfd37b29351ff08f8b27d7507806
grantg2020 <abeach2010@gmail.com>
2019-08-13 15:35:36 -0400

    More

--- configs/getdata.php
+++ configs/getdata.php
@@ -3,6 +3,7 @@ $username="techmeds_FlexSystem";
 $password="BoPest2016";
 $database="techmeds_FlexSystem";
 $mysqli = new mysqli("localhost", $username, $password, $database);
+print "Working";
 $mysqli->select_db($database) or die( "Unable to select database");
 $mysqli->close();
 ?>
