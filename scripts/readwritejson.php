2ef44f2c3f721e80066543bcfc70b8dcae7f2c60
grantg2020 <abeach2010@gmail.com>
2019-08-27 15:13:19 -0400

    Read successfully. Now trying rewriting...

--- scripts/readwritejson.php
+++ scripts/readwritejson.php
@@ -1,16 +1,18 @@
 <?php
 // This PHP script must be in "SOME_PATH/jsonFile/index.php"
 $file = 'temp.json';
-if($_SERVER['REQUEST_METHOD'] === 'POST')
-// or if(!empty($_POST))
-{
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
     file_put_contents($file, $_POST["jsonTxt"]);
-    //may be some error handeling if you want
 }
-else if($_SERVER['REQUEST_METHOD'] === 'GET')
-// or else if(!empty($_GET))
-{
+else if($_SERVER['REQUEST_METHOD'] === 'GET'){
     echo file_get_contents($file);
+    $array_data = json_decode($file, true);
+    $extra = array(
+      'name'    => 'test'
+    );
+    $array_data[] = $extra;
+    $final_data = json_encode($array_data);
+    file_put_contents('temp.json', $final_data);
     //may be some error handeling if you want
 }
 ?>
