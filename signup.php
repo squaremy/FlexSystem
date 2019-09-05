bc81c34504d02addc0b79aa0cf6ff64230c9fd33
squaremy <martinj1@franklinacademy.org>
2019-09-04 22:12:26 -0400

    fixed sign out on other pages?

--- signup.php
+++ signup.php
@@ -37,6 +37,7 @@
 		<!-- </div> -->
 		<button id="confirmsignup" type="button" onclick="confirmsignup()">Sign Up</button>
 	</div>
+	<div class="g-signin2" data-onsuccess="onSignIn" data-onfailure="askForLogin" data-theme="dark" style="visibility: hidden;"></div>
 	<a href="#" style="position: absolute; top:80px; right: 10px;" onclick="logout();">Sign out</a>
 </body>
 <script type="text/javascript" src="scripts/schedule.js"></script>
