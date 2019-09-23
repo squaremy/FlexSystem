fd515ce91c351afa646b6643719ad80ae4db4e03
grantg2020 <abeach2010@gmail.com>
2019-09-23 19:18:43 -0400

    Test

--- scripts/readwritejson.php
+++ scripts/readwritejson.php
@@ -15,35 +15,3 @@ else if($_SERVER['REQUEST_METHOD'] === 'GET'){
     file_put_contents('temp.json', $final_data);
 }
 ?>
-
-<?php
-  $content = mysql_connect("localhost", "root","") or die('Database Not Connected. Please Fix the Issue! ' . mysql_error());
-  mysql_select_db("../database/json.db", $connect);
-
-  $jsonContents = json_get_contents("../configs/data.json");
-
-  $jsonArray = json_decode($jsonContents, true);
-
-  $query;
-
-  for(int i = 0; i < $jsonArray.length; i++) {
-    $name = $jsonArray[i]['name'];
-    $type = $jsonArray[i]['type'];
-    if($type == 'teacher') {
-      $room = $jsonArray[i]['room'];
-      $schedule = $jsonArray[i]['schedule'];
-      $query = "INSERT INTO teacherData(name, type, room, schedule) VALUES('$name', '$type', '$room', '$schedule')";
-    } else {
-      $flexRoom = $jsonArray[i]['flex room'];
-      $schedule = $jsonArray[i]['schedule'];
-      $query = "INSERT INTO teacherData(name, type, flexRoom, schedule) VALUES('$name', '$type', '$flexRoom', '$schedule')";
-    }
-  }
-
-
-  if(!mysql_query($query,$connect)) {
-    die('Error : Query Not Executed. Please Fix the Issue! ' . mysql_error());
-  } else {
-    echo "Data Inserted Successully!!!";
-  }
- ?>
