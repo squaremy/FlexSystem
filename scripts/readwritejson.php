b2681c12e2543daa4a20e234c8ca88c6191fba4e
squaremy <martinj1@franklinacademy.org>
2019-09-26 07:37:45 -0400

    test

--- scripts/readwritejson.php
+++ scripts/readwritejson.php
@@ -23,7 +23,7 @@ else if($_SERVER['REQUEST_METHOD'] === 'GET'){
   $jsonContents = file_get_contents("../configs/GOAL_CONFIG.json");
 
   $jsonArray = json_decode($jsonContents, true);
-  echo count($jsonArray);
+  print $jsonArray;
   $query = null;
 
   for($i = 0; $i < count($jsonArray); $i++) {
