55d1720674d073d355e51dcecd8e9f67f1c3823f
squaremy <martinj1@franklinacademy.org>
2019-10-07 17:21:05 -0400

    clean up

--- schedule.php
+++ schedule.php
@@ -261,8 +261,6 @@
 				window.location.href = 'schedule.php?' + extension;
 			}
 		</script>
-		<!-- <script>loadUser()</script> -->
 		<script type="text/javascript" src="scripts/signin.js"></script>
 	</body>
-<!-- <script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")</script> -->
 </html>
