322d27ea858c289560d5bd165ede000ff173beaf
squaremy <martinj1@franklinacademy.org>
2020-04-06 16:28:36 -0400

    cleared file

--- scripts/test.php
+++ scripts/test.php
@@ -1,4 +1,3 @@
 <?php
-  echo "hash: " . password_hash("Sp33dracer!", PASSWORD_BCRYPT);
-  echo "verify: " . password_verify("Sp33dracer!", '$2y$10$ELelPeMdP0upEvz51P8C3uu7eK597v8p8.N0PSWelOttpGoe8PRUm');
+  
 ?>
